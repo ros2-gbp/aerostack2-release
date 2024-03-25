d36144fc5e4591bd6987f7c2537af936e264ce98
pariaspe <pedro.ariasp@upm.es>
2024-03-25 09:31:41 +0100

    Rebase from 'upstream'

--- as2_core/include/as2_core/utils/tf_utils.hpp
+++ as2_core/include/as2_core/utils/tf_utils.hpp
@@ -184,6 +184,18 @@ public:
     const std::chrono::nanoseconds timeout = TF_TIMEOUT);
 
   /**
+   * @brief convert a geometry_msgs::msg::QuaternionStamped from one frame to another
+   * @param _quaternion a geometry_msgs::msg::QuaternionStamped
+   * @param _target_frame the target frame
+   * @return geometry_msgs::msg::QuaternionStamped in the target frame
+   * @throw tf2::TransformException if the transform is not available
+  */
+  geometry_msgs::msg::QuaternionStamped convert(
+    const geometry_msgs::msg::QuaternionStamped & _quaternion,
+    const std::string & target_frame,
+    const std::chrono::nanoseconds timeout = TF_TIMEOUT);
+
+  /**
    * @brief obtain a PoseStamped from the TF_buffer
    * @param target_frame the target frame
    * @param source_frame the source frame
@@ -213,7 +225,32 @@ public:
    * @param target_frame the target frame
    * @param source_frame the source frame
    * @param time the time of the transform
-   * @return geometry_msgs::msg::TransformStamped
+   * @return geometry_msgs::msg::QuaternionStamped
+   * @throw tf2::TransformException if the transform is not available
+   */
+  geometry_msgs::msg::QuaternionStamped getQuaternionStamped(
+    const std::string & target_frame, const std::string & source_frame,
+    const tf2::TimePoint & time = tf2::TimePointZero,
+    const std::chrono::nanoseconds timeout = TF_TIMEOUT);
+
+  /**
+   * @brief obtain a TransformStamped from the TF_buffer
+   * @param target_frame the target frame
+   * @param source_frame the source frame
+   * @param time the time of the transform
+   * @return geometry_msgs::msg::QuaternionStamped
+   * @throw tf2::TransformException if the transform is not available
+   */
+  geometry_msgs::msg::QuaternionStamped getQuaternionStamped(
+    const std::string & target_frame, const std::string & source_frame, const rclcpp::Time & time,
+    const std::chrono::nanoseconds timeout = TF_TIMEOUT);
+
+  /**
+   * @brief obtain a TransformStamped from the TF_buffer
+   * @param target_frame the target frame
+   * @param source_frame the source frame
+   * @param time the time of the transform
+   * @return geometry_msgs::msg::QuaternionStamped
    * @throw tf2::TransformException if the transform is not available
    */
   geometry_msgs::msg::TransformStamped getTransform(
@@ -265,6 +302,18 @@ public:
    * @param _pose_source_frame
    * @param _timeout
    * @return std::pair<geometry_msgs::msg::PoseStamped, geometry_msgs::msg::TwistStamped>
+   */
+  bool tryConvert(
+    geometry_msgs::msg::QuaternionStamped & _quaternion, const std::string & _target_frame,
+    const std::chrono::nanoseconds timeout = TF_TIMEOUT);
+
+  /**
+   * @brief convert a geometry_msgs::msg::QuaternionStamped to desired frame, checking if frames are
+   * valid
+   * @param _quaternion a geometry_msgs::msg::QuaternionStamped to get converted
+   * @param _target_frame the target frame
+   * @return bool true if the conversion was successful
+   * @throw tf2::TransformException if the transform is not available
    */
   std::pair<geometry_msgs::msg::PoseStamped, geometry_msgs::msg::TwistStamped> getState(
     const geometry_msgs::msg::TwistStamped & _twist, const std::string & _twist_target_frame,
