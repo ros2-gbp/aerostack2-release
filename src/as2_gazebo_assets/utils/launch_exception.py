19abe9a011ab58aeace4d35a0e9d8db6702a388b
pariaspe <pedro.ariasp@upm.es>
2024-08-08 19:49:24 +0200

    Rebase from 'release/humble/as2_gazebo_assets'

--- src/as2_gazebo_assets/utils/launch_exception.py
+++ src/as2_gazebo_assets/utils/launch_exception.py
@@ -25,42 +25,13 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-"""bridge.py."""
 
-from dataclasses import dataclass
-from enum import Enum
+"""Launch exceptions."""
 
-
-class BridgeDirection(Enum):
-    """Enum for bridge directions."""
-
-    BIDIRECTIONAL = 0
-    GZ_TO_ROS = 1
-    ROS_TO_GZ = 2
+__authors__ = 'Pedro Arias Pérez'
+__copyright__ = 'Copyright (c) 2024 Universidad Politécnica de Madrid'
+__license__ = 'BSD-3-Clause'
 
 
-DIRECTION_SYMS = {
-    BridgeDirection.BIDIRECTIONAL: '@',
-    BridgeDirection.GZ_TO_ROS: '[',
-    BridgeDirection.ROS_TO_GZ: ']',
-}
-
-
-@dataclass
-class Bridge:
-    """Bridge Gz<->Ros."""
-
-    gz_topic: str
-    ros_topic: str
-    gz_type: str
-    ros_type: str
-    direction: BridgeDirection
-
-    def argument(self):
-        """Return argument for ros_gz_bridge."""
-        out = f'{self.gz_topic}@{self.ros_type}{DIRECTION_SYMS[self.direction]}{self.gz_type}'
-        return out
-
-    def remapping(self):
-        """Return remapping for ros_gz_bridge."""
-        return (self.gz_topic, self.ros_topic)
+class InvalidSimulationConfigFile(Exception):
+    """Simulation configuration file not supported."""
