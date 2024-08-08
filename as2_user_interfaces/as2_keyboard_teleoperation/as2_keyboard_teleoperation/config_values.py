06b6b61d46749e2692ab7b196cde081807d8367a
pariaspe <pedro.ariasp@upm.es>
2024-08-08 19:47:55 +0200

    Rebase from 'upstream'

--- as2_user_interfaces/as2_keyboard_teleoperation/as2_keyboard_teleoperation/config_values.py
+++ as2_user_interfaces/as2_keyboard_teleoperation/as2_keyboard_teleoperation/config_values.py
@@ -28,10 +28,9 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-__authors__ = "Javier Melero Deza, Pedro Arias Pérez"
-__copyright__ = "Copyright (c) 2022 Universidad Politécnica de Madrid"
-__license__ = "BSD-3-Clause"
-__version__ = "0.1.0"
+__authors__ = 'Javier Melero Deza, Pedro Arias Pérez'
+__copyright__ = 'Copyright (c) 2022 Universidad Politécnica de Madrid'
+__license__ = 'BSD-3-Clause'
 
 from enum import Enum
 
@@ -39,37 +38,54 @@ from enum import Enum
 class ExtendedEnum(Enum):
 
     @classmethod
-    def list(cls):
+    def to_list(cls):
         return list(map(lambda c: c.value, cls))
 
 
 class KeyMappings(ExtendedEnum):
-    TAKE_OFF_KEY = "t"
-    LAND_KEY = "l"
-    HOVER_KEY = "space"
-    EMERGENCY_KEY = "Delete"
-    UP_KEY = "w"
-    DOWN_KEY = "s"
-    ROTATE_RIGHT_KEY = "d"
-    ROTATE_LEFT_KEY = "a"
-    LEFT_KEY = "Left"
-    RIGHT_KEY = "Right"
-    FORWARD_KEY = "Up"
-    BACKWARD_KEY = "Down"
+    TAKE_OFF_KEY = 't'
+    LAND_KEY = 'l'
+    HOVER_KEY = 'space'
+    EMERGENCY_KEY = 'Delete'
+    UP_KEY = 'w'
+    DOWN_KEY = 's'
+    ROTATE_RIGHT_KEY = 'd'
+    ROTATE_LEFT_KEY = 'a'
+    LEFT_KEY = 'Left'
+    RIGHT_KEY = 'Right'
+    FORWARD_KEY = 'Up'
+    BACKWARD_KEY = 'Down'
 
 
-class ControlValues(ExtendedEnum):
-    SPEED_VALUE = 1.00
-    VERTICAL_VALUE = 1.00
-    TURN_SPEED_VALUE = 0.10
+class ControlValues():
+    # Default values
+    SPEED_VALUE = 0.5
+    VERTICAL_VALUE = 0.5
+    TURN_SPEED_VALUE = 0.30
     POSITION_VALUE = 1.00
     ALTITUDE_VALUE = 1.00
-    TURN_ANGLE_VALUE = 0.10
+    TURN_ANGLE_VALUE = 1.57
+
+    @classmethod
+    def initialize(cls, speed_value=None, altitude_speed_value=None, turn_speed_value=None,
+                   position_value=None, altitude_value=None, turn_angle_value=None):
+        if speed_value is not None:
+            cls.SPEED_VALUE = speed_value
+        if altitude_speed_value is not None:
+            cls.VERTICAL_VALUE = altitude_speed_value
+        if turn_speed_value is not None:
+            cls.TURN_SPEED_VALUE = turn_speed_value
+        if position_value is not None:
+            cls.POSITION_VALUE = position_value
+        if altitude_value is not None:
+            cls.ALTITUDE_VALUE = altitude_value
+        if turn_angle_value is not None:
+            cls.TURN_ANGLE_VALUE = turn_angle_value
 
 
 class ControlModes(ExtendedEnum):
-    SPEED_CONTROL = "-SPEED-"
-    POSE_CONTROL = "-POSE-"
+    SPEED_CONTROL = '-SPEED-'
+    POSE_CONTROL = '-POSE-'
 
 
 class Options(ExtendedEnum):
