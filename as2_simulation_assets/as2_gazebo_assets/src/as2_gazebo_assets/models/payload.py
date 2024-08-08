76fb41cf984c8871f679437d1fe7011846a4e6b6
pariaspe <pedro.ariasp@upm.es>
2024-08-08 19:48:17 +0200

    Rebase from 'upstream'

--- as2_simulation_assets/as2_gazebo_assets/src/as2_gazebo_assets/models/payload.py
+++ as2_simulation_assets/as2_gazebo_assets/src/as2_gazebo_assets/models/payload.py
@@ -1,6 +1,4 @@
-"""
-payload.py
-"""
+"""payload.py."""
 
 # Copyright 2022 Universidad Politécnica de Madrid
 #
@@ -32,38 +30,68 @@ payload.py
 
 from __future__ import annotations
 
-__authors__ = "Pedro Arias Pérez"
-__copyright__ = "Copyright (c) 2022 Universidad Politécnica de Madrid"
-__license__ = "BSD-3-Clause"
-__version__ = "0.1.0"
+__authors__ = 'Pedro Arias Pérez'
+__copyright__ = 'Copyright (c) 2022 Universidad Politécnica de Madrid'
+__license__ = 'BSD-3-Clause'
+__version__ = '0.1.0'
 
 
 from enum import Enum
-from typing import Union, List
-from pydantic import validator
-from launch_ros.actions import Node
-from as2_gazebo_assets.bridges.bridge import Bridge
+from typing import ForwardRef, List, Union
+
 from as2_gazebo_assets.bridges import bridges as gz_bridges
 from as2_gazebo_assets.bridges import custom_bridges as gz_custom_bridges
+from as2_gazebo_assets.bridges.bridge import Bridge
 from as2_gazebo_assets.models.entity import Entity
+
+from launch_ros.actions import Node
+
+try:
+    from pydantic.v1 import validator
+except ModuleNotFoundError:
+    from pydantic import validator
 
 
 class CameraTypeEnum(str, Enum):
-    """Valid camera model types"""
+    """Valid camera model types."""
 
-    VGA_CAM = "vga_camera"
-    HD_CAM = "hd_camera"
-    SEMANTIC_CAM = "semantic_camera"
+    VGA_CAM = 'vga_camera'
+    HD_CAM = 'hd_camera'
+    SEMANTIC_CAM = 'semantic_camera'
+
+    @staticmethod
+    def nodes(
+        drone_model_name: str,
+        sensor_model_name: str,
+        sensor_model_type: str,
+        gimbal_name: str,
+        gimbaled: bool
+    ) -> List[Node]:
+        """
+        Return custom bridges (nodes) needed for camera model.
+
+        :param world_name: gz world name
+        :param model_name: gz drone model name
+        :param payload: gz payload (sensor) model type
+        :param sensor_name: gz payload (sensor) model name
+        :param model_prefix: ros model prefix, defaults to ''
+        :return: list with bridges
+        """
+        nodes = [gz_custom_bridges.static_tf_node(
+            drone_model_name, sensor_model_name, sensor_model_type, gimbal_name, gimbaled)
+        ]
+        return nodes
 
     @staticmethod
     def bridges(
         world_name: str,
-        model_name: str,
-        payload: str,
-        sensor_name: str,
-        model_prefix: str = "",
+        drone_model_name: str,
+        sensor_model_name: str,
+        sensor_model_type: str,
+        sensor_model_prefix: str = '',
     ) -> List[Bridge]:
-        """Return bridges needed for camera model
+        """
+        Return bridges needed for camera model.
 
         :param world_name: gz world name
         :param model_name: gz drone model name
@@ -73,28 +101,52 @@ class CameraTypeEnum(str, Enum):
         :return: list with bridges
         """
         bridges = [
-            gz_bridges.image(world_name, model_name, sensor_name,
-                             payload, model_prefix),
-            gz_bridges.camera_info(world_name, model_name,
-                                   sensor_name, payload, model_prefix)
+            gz_bridges.image(world_name, drone_model_name, sensor_model_name,
+                             sensor_model_type, sensor_model_prefix),
+            gz_bridges.camera_info(world_name, drone_model_name, sensor_model_name,
+                                   sensor_model_type, sensor_model_prefix)
         ]
         return bridges
 
 
 class DepthCameraTypeEnum(str, Enum):
-    """Valid depth camera model types"""
+    """Valid depth camera model types."""
 
-    RGBD_CAM = "rgbd_camera"
+    RGBD_CAM = 'rgbd_camera'
+
+    @staticmethod
+    def nodes(
+        drone_model_name: str,
+        sensor_model_name: str,
+        sensor_model_type: str,
+        gimbal_name: str,
+        gimbaled: bool
+    ) -> List[Node]:
+        """
+        Return custom bridges (nodes) needed for camera model.
+
+        :param world_name: gz world name
+        :param model_name: gz drone model name
+        :param payload: gz payload (sensor) model type
+        :param sensor_name: gz payload (sensor) model name
+        :param model_prefix: ros model prefix, defaults to ''
+        :return: list with bridges
+        """
+        nodes = [gz_custom_bridges.static_tf_node(
+            drone_model_name, sensor_model_name, sensor_model_type, gimbal_name, gimbaled)
+        ]
+        return nodes
 
     @staticmethod
     def bridges(
         world_name: str,
-        model_name: str,
-        payload: str,
-        sensor_name: str,
-        model_prefix: str = "",
+        drone_model_name: str,
+        sensor_model_name: str,
+        sensor_model_type: str,
+        sensor_model_prefix: str = '',
     ) -> List[Bridge]:
-        """Return bridges needed for depth camera model
+        """
+        Return bridges needed for depth camera model.
 
         :param world_name: gz world name
         :param model_name: gz drone model name
@@ -104,34 +156,35 @@ class DepthCameraTypeEnum(str, Enum):
         :return: list with bridges
         """
         bridges = [
-            gz_bridges.image(
-                world_name, model_name, sensor_name, payload, model_prefix),
-            gz_bridges.camera_info(
-                world_name, model_name, sensor_name, payload, model_prefix),
-            gz_bridges.depth_image(
-                world_name, model_name, sensor_name, payload, model_prefix),
-            gz_bridges.camera_points(
-                world_name, model_name, sensor_name, payload, model_prefix)
+            gz_bridges.image(world_name, drone_model_name, sensor_model_name,
+                             sensor_model_type, sensor_model_prefix),
+            gz_bridges.camera_info(world_name, drone_model_name, sensor_model_name,
+                                   sensor_model_type, sensor_model_prefix),
+            gz_bridges.depth_image(world_name, drone_model_name, sensor_model_name,
+                                   sensor_model_type, sensor_model_prefix),
+            gz_bridges.camera_points(world_name, drone_model_name, sensor_model_name,
+                                     sensor_model_type, sensor_model_prefix)
         ]
         return bridges
 
 
 class LidarTypeEnum(str, Enum):
-    """Valid lidar model types"""
+    """Valid lidar model types."""
 
-    POINT_LIDAR = "point_lidar"  # FIXME: not working
-    PLANAR_LIDAR = "planar_lidar"
-    LIDAR_3D = "lidar_3d"
+    POINT_LIDAR = 'point_lidar'  # FIXME: not working
+    PLANAR_LIDAR = 'planar_lidar'
+    LIDAR_3D = 'lidar_3d'
 
     @staticmethod
     def bridges(
         world_name: str,
-        model_name: str,
-        payload: str,
-        sensor_name: str,
-        model_prefix: str = "",
+        drone_model_name: str,
+        sensor_model_name: str,
+        sensor_model_type: str,
+        sensor_model_prefix: str = '',
     ) -> List[Bridge]:
-        """Return bridges needed for lidar model
+        """
+        Return bridges needed for lidar model.
 
         :param world_name: gz world name
         :param model_name: gz drone model name
@@ -142,17 +195,19 @@ class LidarTypeEnum(str, Enum):
         """
         bridges = [
             gz_bridges.lidar_scan(
-                world_name, model_name, sensor_name, payload, model_prefix),
+                world_name, drone_model_name, sensor_model_name,
+                sensor_model_type, sensor_model_prefix),
             gz_bridges.lidar_points(
-                world_name, model_name, sensor_name, payload, model_prefix)
+                world_name, drone_model_name, sensor_model_name,
+                sensor_model_type, sensor_model_prefix)
         ]
         return bridges
 
 
 class GpsTypeEnum(str, Enum):
-    """Valid GPS model types"""
+    """Valid GPS model types."""
 
-    GPS = "gps"
+    GPS = 'gps'
 
     @staticmethod
     def nodes(
@@ -160,9 +215,10 @@ class GpsTypeEnum(str, Enum):
         model_name: str,
         payload: str,
         sensor_name: str,
-        model_prefix: str = "",
+        model_prefix: str = '',
     ) -> List[Node]:
-        """Return custom bridges (nodes) needed for gps model
+        """
+        Return custom bridges (nodes) needed for gps model.
 
         :param world_name: gz world name
         :param model_name: gz drone model name
@@ -182,9 +238,10 @@ class GpsTypeEnum(str, Enum):
         model_name: str,
         payload: str,
         sensor_name: str,
-        model_prefix: str = "",
+        model_prefix: str = '',
     ) -> List[Bridge]:
-        """Return bridges needed for gps model
+        """
+        Return bridges needed for gps model.
 
         :param world_name: gz world name
         :param model_name: gz drone model name
@@ -202,9 +259,9 @@ class GpsTypeEnum(str, Enum):
 
 
 class GripperTypeEnum(str, Enum):
-    """Valid gripper model types"""
+    """Valid gripper model types."""
 
-    SUCTION_GRIPPER = "suction_gripper"
+    SUCTION_GRIPPER = 'suction_gripper'
 
     @staticmethod
     def bridges(
@@ -212,9 +269,10 @@ class GripperTypeEnum(str, Enum):
         model_name: str,
         payload: str,
         sensor_name: str,
-        model_prefix: str = "",
+        model_prefix: str = '',
     ) -> List[Bridge]:
-        """Return bridges needed for gripper model
+        """
+        Return bridges needed for gripper model.
 
         :param world_name: gz world name
         :param model_name: gz drone model name
@@ -235,20 +293,21 @@ class GripperTypeEnum(str, Enum):
 
 
 class GimbalTypeEnum(str, Enum):
-    """Valid gimbal model types"""
+    """Valid gimbal model types."""
 
-    GIMBAL_SPEED = "gimbal_speed"
-    GIMBAL_POSITION = "gimbal_position"
+    GIMBAL_SPEED = 'gimbal_speed'
+    GIMBAL_POSITION = 'gimbal_position'
 
     @staticmethod
     def nodes(
         world_name: str,
-        model_name: str,
-        sensor_name: str,
-        gimbal_name: str,
-        model_type: str,
+        drone_model_name: str,
+        sensor_model_name: str,
+        gimbal_model_name: str,
+        gimbal_model_type: str,
     ) -> List[Node]:
-        """Return custom bridges (nodes) needed for gps model
+        """
+        Return custom bridges (nodes) needed for gps model.
 
         :param world_name: gz world name
         :param model_name: gz drone model name
@@ -257,24 +316,29 @@ class GimbalTypeEnum(str, Enum):
         :param model_prefix: ros model prefix, defaults to ''
         :return: list with bridges
         """
-
-        gimbal_type = (
-            "position"
-            if model_type == GimbalTypeEnum.GIMBAL_POSITION.value
-            else "speed"
+        gimbal_model_type = (
+            'position'
+            if gimbal_model_type == GimbalTypeEnum.GIMBAL_POSITION.value
+            else 'speed'
         )
 
         nodes = [
             gz_custom_bridges.gimbal_node(
-                world_name, model_name, sensor_name, gimbal_name, gimbal_type
+                world_name, drone_model_name, sensor_model_name,
+                gimbal_model_name, gimbal_model_type
             )
         ]
 
         return nodes
 
 
+# Forward reference needed for pydantic==1.8.*. Newer versions don't need it
+Payload = ForwardRef('Payload')
+
+
 class Payload(Entity):
-    """Gz Payload Entity
+    """
+    Gz Payload Entity.
 
     Use model_type as sensor_type
     """
@@ -282,16 +346,20 @@ class Payload(Entity):
     model_type: Union[
         CameraTypeEnum, DepthCameraTypeEnum, LidarTypeEnum, GpsTypeEnum, GimbalTypeEnum
     ] = None
-    sensor_attached: str = "None"
+    sensor_attached: str = 'None'
+    sensor_attached_type: str = 'None'
     payload: Payload = None
     gimbaled: bool = False
+    gimbal_name: str = 'None'
 
-    @validator("payload", always=True)
+    @validator('payload', always=True)
     def set_gimbaled_default(cls, v, values):
-        if "model_type" in values and isinstance(values["model_type"], GimbalTypeEnum):
+        if 'model_type' in values and isinstance(values['model_type'], GimbalTypeEnum):
             if v is not None:
-                values["sensor_attached"] = v.model_name
+                values['sensor_attached'] = v.model_name
+                values['sensor_attached_type'] = v.model_type
                 v.gimbaled = True
+                v.gimbal_name = values['model_name']
             else:
                 raise ValueError(
                     f"{values['model_name']}({values['model_type']}): Missing field 'payload'"
@@ -307,17 +375,27 @@ class Payload(Entity):
     #     validate_assignment = True
 
     def bridges(self, world_name, drone_model_name) -> tuple[List[Bridge], List[Node]]:
-        """Return bridges from payload model
+        """
+        Return bridges from payload model.
 
         :param world_name: world name
         :param drone_model_name: drone model name
         :param model_prefix: ros topic prefix name, defaults to ''
-        :return ([bridges], [nodes])
-        bridges -> standard bridges
-        nodes -> custom bridges
+            :return ([bridges], [nodes]) bridges -> standard bridges
+            nodes -> custom bridges
         """
         bridges = []
         nodes = []
+
+        if isinstance(self.model_type, CameraTypeEnum) or \
+                isinstance(self.model_type, DepthCameraTypeEnum):
+            nodes = self.model_type.nodes(
+                drone_model_name,
+                self.model_name,
+                self.model_type.value,
+                self.gimbal_name,
+                self.gimbaled
+            )
 
         if isinstance(self.model_type, GpsTypeEnum):
             # FIXME: current version of standard gz navsat bridge is not working properly
@@ -336,14 +414,22 @@ class Payload(Entity):
             )
 
         else:
-            sensor_name = self.model_type.value
+            sensor_model_name = self.model_name
+            sensor_model_type = self.model_type.value
+            sensor_model_prefix = sensor_model_name
             if self.gimbaled:
-                sensor_name = "gb/model/_0/model/_1/model/_2/model/" + self.model_type.value
+                sensor_model_name = self.gimbal_name + '/model/_0/model/_1/model/_2/model/' + \
+                    sensor_model_name
+                sensor_model_prefix = self.gimbal_name + '/' + self.model_name
 
             bridges = self.model_type.bridges(world_name, drone_model_name,
-                                              self.model_name, sensor_name, self.model_name)
+                                              sensor_model_name, sensor_model_type,
+                                              sensor_model_prefix)
         return bridges, nodes
 
     def generate(self, world) -> tuple[str, str]:
-        """Not model generated from payload, use drone instead"""
-        return "", ""
+        """Not model generated from payload, use drone instead."""
+        return '', ''
+
+
+Payload.update_forward_refs()
