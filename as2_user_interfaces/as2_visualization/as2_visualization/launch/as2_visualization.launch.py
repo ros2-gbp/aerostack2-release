633aa5fe433125f81292f3d7bcff40d7a7716910
pariaspe <pedro.ariasp@upm.es>
2025-07-23 20:07:16 +0200

    Rebase from 'upstream'

--- as2_user_interfaces/as2_visualization/as2_visualization/launch/as2_visualization.launch.py
+++ as2_user_interfaces/as2_visualization/as2_visualization/launch/as2_visualization.launch.py
@@ -35,23 +35,87 @@ __copyright__ = 'Copyright (c) 2024 Universidad Politécnica de Madrid'
 __license__ = 'BSD-3-Clause'
 
 import os
+from pathlib import Path
+from xml.etree import ElementTree
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchContext, LaunchDescription
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
+from launch_ros.actions import Node, SetParameter
+
+
+def sdf2viz(sdf_file: str):
+    """Create SDF compatible to URDF conversion."""
+    tree = ElementTree.parse(sdf_file)
+
+    model = tree.find('./model')
+    model_pose = model.find('./pose')
+    if model_pose is not None:
+        model.remove(model_pose)
+
+    # Get path to the models
+    sdf_base_path = os.path.dirname(os.path.dirname(sdf_file))
+    # Replace all <uri> elements
+    for uri in tree.findall('.//uri'):
+        if uri.text.startswith('model://'):
+            # Extract the relative path after 'model://'
+            relative_path = uri.text.split('model://')[-1]
+            # Build the absolute path
+            absolute_path = os.path.join(sdf_base_path, relative_path)
+            # Update the URI text
+            if os.path.exists(absolute_path):
+                uri.text = f'file://{absolute_path}'
+            else:
+                raise FileNotFoundError(f'{absolute_path} not found')
+
+    for sensor_model in tree.findall('.//sensor/../..'):
+        sensor_name = sensor_model.attrib['name']
+        model.remove(sensor_model)
+
+        for joint in model.iter('joint'):
+            if joint.find('./child').text == sensor_name:
+                model.remove(joint)
+
+    # TODO(pariaspe): Avoid saving file and return directly the string
+    # return ElementTree.dump(tree)
+
+    new_file = os.path.join('/tmp/', os.path.basename(sdf_file))
+    tree.write(new_file)
+    return new_file
+
+
+def get_model_path(model_name: str) -> Path:
+    """Return Path of model sdf file."""
+    # Concatenate the model directory and the GZ_SIM_RESOURCE_PATH environment variable
+    model_dir = Path(get_package_share_directory(
+        'as2_gazebo_assets'), 'models')
+    resource_path = os.environ.get('GZ_SIM_RESOURCE_PATH')
+
+    paths = [model_dir]
+    if resource_path:
+        paths += [Path(p) for p in resource_path.split(':')]
+
+    # Define the filename to look for
+    filename = f'{model_name}/{model_name}.sdf'
+
+    # Loop through each directory and check if the file exists
+    for path in paths:
+        filepath = path / filename
+        if filepath.is_file():
+            # If the file exists, return the path
+            return filepath
+    raise FileNotFoundError(
+        f'{filename} not found in {paths}. Does the model sdf template exists?'
+    )
 
 
 def generate_robot_state_publisher(context: LaunchContext):
     """Publish drone URDF."""
-    sdf_file = os.path.join(get_package_share_directory(
-        'as2_gazebo_assets'),
-        'models', LaunchConfiguration('drone_model').perform(context),
-        LaunchConfiguration('drone_model').perform(context) + '_viz.sdf')
+    sdf_file = get_model_path(LaunchConfiguration('drone_model').perform(context))
 
-    with open(sdf_file, 'r', encoding='utf-8') as info:
+    with open(sdf2viz(sdf_file), 'r', encoding='utf-8') as info:
         robot_desc = info.read()
 
     robot_state_publisher = Node(
@@ -128,9 +192,9 @@ def generate_launch_description():
             ),
             DeclareLaunchArgument(
                 'drone_model',
-                choices=['quadrotor_base', 'crazyflie'],
                 default_value='quadrotor_base',
-                description='Drone model to visualize.'
+                description='Drone model to visualize. Available options: quadrotor_base,'
+                ' hexrotor_base, crazyflie, x500, px4vision, or use your own model.',
             ),
             DeclareLaunchArgument(
                 'paint_markers',
@@ -150,6 +214,7 @@ def generate_launch_description():
                 description='Length for last poses.',
             ),
             OpaqueFunction(function=generate_robot_state_publisher),
+            SetParameter(name='use_sim_time', value=LaunchConfiguration('use_sim_time')),
             rviz,
             viz_markers,
             viz_geozones,
